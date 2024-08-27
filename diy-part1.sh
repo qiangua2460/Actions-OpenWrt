8e3f287428d04d40de01697086c49d5fac0edebe
qiangua2460 <55632404+qiangua2460@users.noreply.github.com>
2024-08-28 01:22:52 +0800

    更新 .config

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,3 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
